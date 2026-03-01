0c93266056c93bd2cd37a014d37e31695d02cc11
Shadw-Developer <alissonfarias920@gmail.com>
2026-03-01 14:12:47 -0300

    Update error message for root element not found

--- index.tsx
+++ index.tsx
@@ -4,7 +4,7 @@ import App from './src/App';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+  throw new Error("Não foi possível encontrar o elemento raiz para montar.");
 }
 
 const root = ReactDOM.createRoot(rootElement);
